02b9f09cff04332c28e4af889860dad8b4e79e13
Abhishek6416 <abhishekkr6416@gmail.com>
2023-01-20 21:28:43 +0530

    myPortfolio:UsingReactJS

---
+++ Portfolio/src/Utils/Projects.js
@@ -0,0 +1,46 @@
+
+import Hotstar from "../ash/Hotstar.png"
+import OYO from "../ash/OYO.png"
+import PharmEasy1 from "../ash/PharmEasy1.png"
+import fitnesspal from "../ash/fitnesspal.png"
+export const projects = [
+
+	{
+		img: Hotstar,
+		title: 'Hotstar Clone ',
+		des: 'Disney+ Hotstar is an online video streaming platform owned by Novi Digital Entertainment Private Limited, a wholly owned subsidiary of Star India Private Limited Imported components and exported as well, searching, sorting in Json server, and it is completely dynamic',
+		live: 'https://anjali280.github.io/hotstart_clone_/',
+		demo: '',
+		gitHub: 'https://github.com/Abhishek6416/Hotstar-Clone',
+		technologies: ['HTML', 'CSS', 'javaScript','GitHub','Json Server'],
+	},
+	{
+		img: OYO,
+		title: 'OYO clone',
+		des: 'OYO is a global platform that empowers entrepreneurs and small businesses with hotels and homes by providing full stack technology To build a brand loved by both travellers & city dwellers ',
+		demo: '',
+		live: 'https://oyo-rooms-replica.netlify.app/',
+		gitHub: 'https://github.com/PankuMehra/OyoRooms_Replica/tree/master/OyoRooms',
+		technologies: ['HTML', 'CSS', 'javaScript','Json Server',"React","Redux","Material UI","Chakra UI"],
+	},
+
+	{
+		img: fitnesspal,
+		title: 'MyFitnessPal clone',
+		des: 'MyFitness pal is an fitness app that helps you to manage your diet plans for loose or gain weight according to the current health status of your body',
+		demo: '',
+		live: 'https://myfitnesspal1.netlify.app/',
+		gitHub: 'https://github.com/bewithkomalkumar/MyFitnessPal_Clone',
+		technologies: ['HTML', 'CSS', 'javaScript','Json Server',"React","Redux","Material UI","Chakra UI"]
+	},
+
+	{
+		img: PharmEasy1,
+		title: 'Pharmeasy clone',
+		des: 'PharmEasy is a consumer healthcare “super app” that provides consumers with on-demand, home delivered access to a wide range of prescription Data is coming from local storage and it is completely dynamic',
+		demo: '',
+		live: 'https://pharmeasyclone2022.netlify.app/',
+		gitHub: 'https://github.com/Dheeraj-Bhandari/PharmEasy-Clone',
+		technologies: ['HTML', 'CSS', 'javaScript','GitHub','Json Server','Bootstrap'],
+	},
+];
